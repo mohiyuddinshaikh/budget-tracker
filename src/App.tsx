c21a5346d283a7aa9e012959e4d2a6ce9259aa1d
mohiyuddin <mohiyuddin.shaikh@tradofina.com>
2025-10-31 11:21:32 +0530

    shad cn setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,6 +30,10 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p className="text-red-500">Hellow world</p>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </>
   );
 }
