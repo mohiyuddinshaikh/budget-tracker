541e83bf5e4cda95b082951727f213aee514a847
Rupali Vide <rupali.vide@tradofina.com>
2025-11-19 16:36:42 +0530

    login - firebase implmentation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import google from "../../assets/images/google.svg";
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
+import { auth, googleProvider } from "../../firebase";
+import { signInWithPopup } from "firebase/auth";
 
 export default function Login() {
   const navigate = useNavigate();
+  const handleGoogleLogin = async () => {
+    console.log("first");
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log("User:", result.user);
+      navigate("/home");
+      // alert("Login Successful!");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
-
     <div className="min-h-screen flex items-center justify-center bg-white px-4">
       <Card className="w-[530px] max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl p-6 sm:p-8 text-center shadow-lg rounded-2xl">
         <CardContent className="flex flex-col items-center">
@@ -24,9 +36,14 @@ export default function Login() {
           <Button
             variant="outline"
             className="w-full sm:w-[280px] md:w-[320px] flex items-center justify-center gap-5 border rounded-xl py-3 sm:py-4 md:py-5 text-gray-700 font-medium"
-            onClick={() => navigate("/home")}
+            // onClick={() => navigate("/home")}
+            onClick={handleGoogleLogin}
           >
-            <img src={google} alt="Google logo" className="w-6 h-6 sm:w-8 sm:h-8" />
+            <img
+              src={google}
+              alt="Google logo"
+              className="w-6 h-6 sm:w-8 sm:h-8"
+            />
             Sign in with Google
           </Button>
         </CardContent>
